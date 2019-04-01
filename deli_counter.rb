814d764a044314f7576f9449dcadde7e3d2aaba6
NAM NGUYEN <namdevlife@gmail.com>
2019-04-01 01:56:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,8 @@ def line(deli)
   puts "The line is currently empty."
     else
     current_line = "The line is currently:"
-    deli.each_with_index(1) do |
+    deli.each_with_index(1) do |person, i|
+      
   
   
 end

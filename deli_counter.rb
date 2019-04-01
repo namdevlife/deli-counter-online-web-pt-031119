38956065c58f985a8c4be130e7acbe7439de3476
NAM NGUYEN <namdevlife@gmail.com>
2019-04-01 02:07:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,14 +16,4 @@ end
 
 
 
-def take_a_number(katz_deli, name_of_the_person)
-  name_of_the_person.each_with_index.map do |name_of_the_person, index|
-    puts "#{katz_deli}" + "#{index+1}!"
-  end
-end
 
-def now_serving(next_person)
-  next_person.pop
-  if next_person == 0
-  puts "There is nobody waiting to be served!"
-end

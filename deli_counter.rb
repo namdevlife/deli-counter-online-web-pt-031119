a3d72f593f4cced9cddc7d945b8ba9a7256d98ab
NAM NGUYEN <namdevlife@gmail.com>
2019-04-01 01:30:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(customer)
+    if line == 0  
+  puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name_of_the_person)
+  name_of_the_person.each_with_index.map do |name_of_the_person, index|
+    puts "#{katz_deli}" + "#{index+1}!"
+  end
+end
+
+def now_serving(next_person)
+  next_person.pop
+  if next_person == 0
+  puts "There is nobody waiting to be served!"
+end
